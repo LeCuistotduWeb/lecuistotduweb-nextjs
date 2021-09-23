bef548f63ea4c093ba072c0fc087a6975b0203a0
Gaetan Boyron <gaetan.boyron@birchbox.com>
2021-09-23 13:54:34 +0200

    wip admin page

---
+++ pages/admin/index.js
@@ -0,0 +1,22 @@
+import React, {} from 'react'
+import styles from "../../styles/layouts/admin.module.scss"
+import AdminLayout from "../../layouts/AdminLayout";
+
+const AdminDashboard = (props) => {
+    return (
+        <AdminLayout title={"Dashboard"}>
+            <section className={styles.grid}>
+                <article className={styles.article}></article>
+                <article className={styles.article}></article>
+                <article className={styles.article}></article>
+                <article className={styles.article}></article>
+                <article className={styles.article}></article>
+                <article className={styles.article}></article>
+                <article className={styles.article}></article>
+                <article className={styles.article}></article>
+            </section>
+        </AdminLayout>
+    );
+}
+
+export default AdminDashboard
