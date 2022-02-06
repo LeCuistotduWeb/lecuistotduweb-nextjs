b2f8e1ff0cb060017f823980d3bac48ae5e7bfcf
Gaetan Boyron <gaetan.boyron@blissim.fr>
2022-02-06 13:53:33 +0100

    fix: fetch url

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 const Home = (props) => {
 
-    const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/works`, fetcher)
+    const { data, error } = useSWR(`/api/works`, fetcher)
 
     return (
         <DefaultLayout>
