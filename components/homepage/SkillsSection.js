41527f42c755296e1cbfdd9c3a5f98a8acf7603b
lecuistotduweb <gaetan.boyron@gmail.com>
2022-06-18 18:25:02 +0200

    framer animation

--- components/homepage/SkillsSection.js
+++ components/homepage/SkillsSection.js
@@ -1,11 +1,31 @@
 import * as React from 'react';
-import Image from "next/image";
+import { motion } from "framer-motion"
 
 const SkillsSection = (props) => {
 
-    const sizeLogo = 60;
-
     const toolsAndSkillData = ["HTML5", "CSS3", "SCSS", "Javascript", "ReactJs", "NextJs", "PHP", "Mysql", "Symfony 3/4", "Adobe suite", "Figma", "Wordpress", "NPM", "Vercel", "Git",]
+
+    const listAnimation = {
+        hidden: { opacity: 0 },
+        show: {
+            opacity: 1,
+            transition: {
+                delayChildren: 0.5,
+                staggerChildren: 0.05,
+            }
+        }
+    }
+
+    const item = {
+     hidden: { 
+        opacity: 0,
+        scale: 0,
+    },
+     show: { 
+        opacity: 1,
+        scale: [0,1.05,1], 
+        }
+    }
 
     return (
         <section className="skills section" id={"skills"}>
@@ -13,77 +33,12 @@ const SkillsSection = (props) => {
                 <h2 className="section-title">Technologies et outils</h2>
 
                 <div className="skillsList">
-                    <ul>
+                    <motion.ul initial="hidden" animate="show" variants={listAnimation}>
                         {toolsAndSkillData.map((skill,index) => (
-                            <li key={index}>{skill}</li>
+                            <motion.li  variants={item} key={index}>{skill}</motion.li>
                         ))}
-                    </ul>
+                    </motion.ul>
                 </div>
-
-                {/*<div className="logo-list">*/}
-                {/*    <ul>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/html5-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/css3-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/javascript-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/jquery-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/php-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/mysql-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/npm-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/yarn-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/sass-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/symfony-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/wordpress-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*        <li>*/}
-                {/*            <div className="card card-skill">*/}
-                {/*                <Image className="logo" width={sizeLogo} height={sizeLogo} src="/images/skills/react-logo.png" alt="logo html 5"/>*/}
-                {/*            </div>*/}
-                {/*        </li>*/}
-                {/*    </ul>*/}
-                {/*</div>*/}
             </div>
         </section>
     )
