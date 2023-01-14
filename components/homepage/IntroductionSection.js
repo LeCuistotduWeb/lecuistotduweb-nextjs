32c0bd96f35c3dabd0badd5f378d85c7c5b54c72
lecuistotduweb <gaetan.boyron@gmail.com>
2023-01-14 20:54:32 +0100

    add my cv to the first section of the homepage

--- components/homepage/IntroductionSection.js
+++ components/homepage/IntroductionSection.js
@@ -1,32 +1,50 @@
-import * as React from 'react';
+import * as React from "react";
 import Link from "next/link";
 import ProfileSvg from "../profile/Profile";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 const IntroductionSection = (props) => {
-    return (
-        <section className={"introduction container section"}>
-            <div className={"introduction-image"}>
-                <ProfileSvg type="default" />
-            </div>
-            <div className={"introduction-content"}>
-                {/*<div>Hello ! Je suis gaëtan Boyron alias</div>*/}
-                <motion.p initial={{ opacity: 0, y: 15 }}
-                          animate={{ opacity: 1, y: 0 }}
-                          transition={{
-                            duration: .3,
-                            delay: 0,
-                          }} className="subtitle">Développeur Front-End</motion.p>
-                <h1 className="section-title">LeCuistotduWeb.</h1>
-                <p >Bienvenue sur mon portfolio. Je m'appelle Gaëtan Boyron (alias LeCuistotduWeb). Animé par l’envie de développer ma créativité et de paticiper à la conception du web de demain, j’ai décidé de me reconvertir en 2017 afin de vivre de ma passion : le développement Web.</p>
-                <div>
-                    <Link href="/experiences" passHref>
-                        <a className="btn btn-secondary">Mes expériences</a>
-                    </Link>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className={"introduction container section"}>
+      <div className={"introduction-image"}>
+        <ProfileSvg type="default" />
+      </div>
+      <div className={"introduction-content"}>
+        {/*<div>Hello ! Je suis gaëtan Boyron alias</div>*/}
+        <motion.p
+          initial={{ opacity: 0, y: 15 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 0.3,
+            delay: 0,
+          }}
+          className="subtitle"
+        >
+          Développeur Front-End
+        </motion.p>
+        <h1 className="section-title">LeCuistotduWeb.</h1>
+        <p>
+          Bienvenue sur mon portfolio. Je m'appelle{" "}
+          <strong>Gaëtan Boyron</strong> (alias LeCuistotduWeb). Animé par
+          l’envie de développer ma créativité et de paticiper à la conception du
+          web de demain, j’ai décidé de me reconvertir en 2017 afin de vivre de
+          ma passion : le développement Web.
+        </p>
+        <div className="btns-container">
+          <Link href="/experiences" passHref>
+            <a className="btn btn-secondary">Mes expériences</a>
+          </Link>
+          <a
+            href="/pdf/cv-2022.pdf"
+            className="btn"
+            target="_blank"
+          >
+            Télécharger mon CV
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default IntroductionSection
+export default IntroductionSection;
