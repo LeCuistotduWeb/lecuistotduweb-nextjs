66a94bd7776ece2fd5b0a61d1fb452e984a49f29
lecuistotduweb <gaetan.boyron@gmail.com>
2023-05-14 16:32:45 +0200

    fix: fix ga script and packages upgrades

--- pages/_document.js
+++ pages/_document.js
@@ -10,16 +10,21 @@ class MyDocument extends Document {
         return (
             <Html lang="fr">
                 <Head>
-                  <link
-                    href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+                        rel="stylesheet"
+                    />
                   {/* Global Site Tag (gtag.js) - Google Analytics */}
                   {(process.env.NEXT_PUBLIC_ENV === 'production') && (
-                  <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
-                  <script dangerouslySetInnerHTML={{__html: 
-                    `window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`}}/>
+                    <>
+                      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+                      <script dangerouslySetInnerHTML={{ __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});`,
+                      }}/>
+                    </>
                   )}
                 </Head>
                 <body>
