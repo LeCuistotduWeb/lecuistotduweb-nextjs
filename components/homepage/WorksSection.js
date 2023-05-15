013d6110123d51b2e8bcb03b0286a83b0e3d1b0e
lecuistotduweb <gaetan.boyron@gmail.com>
2023-05-15 08:37:29 +0200

    Revert "fix: update mariejade work"
    This reverts commit 96c12df8acd36463505b1d3b736be508c08737fb.

--- components/homepage/WorksSection.js
+++ components/homepage/WorksSection.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
 import LatestWorksCarousel from '../works/LatestWorksCarousel'
-import WorkCard from '../cards/WorkCard';
 
 const WorksSection = ({works}) => {
     return (
@@ -12,15 +11,9 @@ const WorksSection = ({works}) => {
                     <p>Vous voulez en savoir plus sur les différentes expériences professionnelles et formations que je mène et celle que j'ai pu mener ?</p>
                     <Link href="/works" className="btn btn-secondary">Mes réalisations</Link>
                 </div>
-            
-                <div className="latest-works-container">
-                    <div className="work-1">
-                        <WorkCard work={works[0]}/>
-                    </div>
-                    <div className="work-2">
-                        <WorkCard work={works[1]}/>
-                    </div>
-                </div>
+            </div>
+            <div className="latest-works-carousel-container">
+                <LatestWorksCarousel works={works}/>
             </div>
         </section>
     )
