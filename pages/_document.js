664a87ccda3c86ff60e3dfacde3ba01e357acff6
lecuistotduweb <gaetan.boyron@gmail.com>
2023-06-17 14:15:44 +0200

    fix fix viewport

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
         return (
             <Html lang="fr">
                 <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
                         rel="stylesheet"
