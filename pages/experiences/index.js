e19861ab024fd4a4c189a28b9c9b6ab6e01e384e
lecuistotduweb <gaetan.boyron@gmail.com>
2023-09-22 22:40:59 +0200

    feat: add experience and add border radiant

--- pages/experiences/index.js
+++ pages/experiences/index.js
@@ -5,144 +5,140 @@ import ExperienceCards from "../../components/cards/ExperienceCards";
 import Link from "next/link";
 
 const experiencesData = [
-    {
-        id: 10,
-        type: "title",
-        title: "2019 / Aujourd'hui",
-    },
-    {
-        id: 9,
-        type: "card",
-        date: "2019 / Aujourd'hui",
-        title: "Développeur Web",
-        company: "Blissim.fr - ex Birchbox.fr (Paris)",
-        content: <>
-            <ul>
-                <li>- Migration du site sur une PWA Nextjs</li>
-                <li>- Développement de nouvelles fonctionnalités</li>
-                <li>- Développement d'outil interne</li>
-            </ul>
-        </>
-    },
-    {
-        id: 8,
-        type: "title",
-        title: "2018",
-    },
-    {
-        id: 7,
-        type: "card",
-        date: "2018 / 2019 (octobre-avril)",
-        title: "Stagiaire développeur web",
-        company: "Nomad Education (Paris)",
-        content: <>
-            <ul>
-                <li>
-                    - Refonte du site vitrine.
-                </li>
-                <li>
-                    - Aide au développement de nouvelles fonctionnalités sur
-                </li>
-            </ul>
-        </>
-    },
-    {
-        id: 6,
-        type: "title",
-        title: "2017",
-    },
-    {
-        id: 5,
-        type: "card",
-        date: "2017 / Ajourd’hui",
-        title: "Développeur fullstack / Webdesigner",
-        company: "lepoudrierdefleur.fr",
-        content: <>
-            <ul>
-                <li>
-                    - Maintenance du site Web.
-                </li>
-                <li>
-                    - Développement de nouvelles fonctionnalités
-                </li>
-                <li>
-                    - Montage vidéos, retouches photos, créations de visuels
-                </li>
-            </ul>
-        </>
-    },
-    {
-        id: 4,
-        type: "card",
-        date: "2017 (mai-juin)",
-        title: "Stagiaire webdesigner",
-        company: "Arkade Communication (Valence, Drôme)",
-        content: <>
-            <ul>
-                <li>
-                    - Amelioration d’UX/UI de site-Web.
-                </li>
-                <li>
-                    - Correction de bugs d’un site basé sous Wordpress.
-                </li>
-                <li>
-                    - Covering de vehicule utilitaire.
-                </li>
-            </ul>
-        </>
-    },
-    {
-        id: 3,
-        type: "title",
-        title: "2015",
-    },
-    {
-        id: 2,
-        type: "card",
-        date: "2015 / 2016",
-        title: "Conducteur de machine de conditionnement en agroalimentaire",
-        company: "Société Charles & Alice (Alex, Drôme)",
-        content: <>
-            <ul>
-                <li>
-                    - Respect des enjeux de productivité, qualité, hygiène et
-                    sécurité.
-                </li>
-                <li>
-                    - Contrôle, diagnostique et communication des pannes.
-                </li>
-                <li>
-                    - Proposition d’améliorations du fonctionnement de la ligne.
-                </li>
-            </ul>
-        </>
-    },
-    {
-        id: 1,
-        type: "title",
-        title: "2013",
-    },
-    {
-        id: 0,
-        type: "card",
-        date: "2013",
-        title: "Chef de restauration",
-        company: "école municipale (Vaulx, Haute Savoie)",
-        content: <>
-            <ul>
-                <li>
-                    - Élaboration des menus.
-                </li>
-                <li>
-                    - Gestion des stocks.
-                </li>
-                <li>
-                    - Relations fournisseurs.
-                </li>
-            </ul>
-        </>
-    }
-]
+  {
+    id: 11,
+    type: "title",
+    title: "2023 à Aujourd'hui",
+  },
+  {
+    id: 10,
+    type: "card",
+    date: "2023 / Aujourd'hui",
+    title: "Développeur Front / Consultant",
+    company: "L'Atelier - Société de conseil (Paris)",
+    content: "",
+  },
+  {
+    id: 11,
+    type: "title",
+    title: "2019 / 2022",
+  },
+  {
+    id: 9,
+    type: "card",
+    date: "2019 / 2022",
+    title: "Développeur Web",
+    company: "Blissim.fr - ex Birchbox.fr (Paris)",
+    content: (
+      <>
+        <ul>
+          <li>- Migration du site sur une PWA Nextjs</li>
+          <li>- Développement de nouvelles fonctionnalités</li>
+          <li>- Développement d'outil interne</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    id: 8,
+    type: "title",
+    title: "2018",
+  },
+  {
+    id: 7,
+    type: "card",
+    date: "2018 / 2019 (octobre-avril)",
+    title: "Stagiaire développeur web",
+    company: "Nomad Education (Paris)",
+    content: (
+      <>
+        <ul>
+          <li>- Refonte du site vitrine.</li>
+          <li>- Aide au développement de nouvelles fonctionnalités sur</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    id: 6,
+    type: "title",
+    title: "2017",
+  },
+  {
+    id: 5,
+    type: "card",
+    date: "2017 / Ajourd’hui",
+    title: "Développeur fullstack / Webdesigner",
+    company: "lepoudrierdefleur.fr",
+    content: (
+      <>
+        <ul>
+          <li>- Maintenance du site Web.</li>
+          <li>- Développement de nouvelles fonctionnalités</li>
+          <li>- Montage vidéos, retouches photos, créations de visuels</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    id: 4,
+    type: "card",
+    date: "2017 (mai-juin)",
+    title: "Stagiaire webdesigner",
+    company: "Arkade Communication (Valence, Drôme)",
+    content: (
+      <>
+        <ul>
+          <li>- Amelioration d’UX/UI de site-Web.</li>
+          <li>- Correction de bugs d’un site basé sous Wordpress.</li>
+          <li>- Covering de vehicule utilitaire.</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    id: 3,
+    type: "title",
+    title: "2015",
+  },
+  {
+    id: 2,
+    type: "card",
+    date: "2015 / 2016",
+    title: "Conducteur de machine de conditionnement en agroalimentaire",
+    company: "Société Charles & Alice (Alex, Drôme)",
+    content: (
+      <>
+        <ul>
+          <li>- Respect des enjeux de productivité, qualité, hygiène et sécurité.</li>
+          <li>- Contrôle, diagnostique et communication des pannes.</li>
+          <li>- Proposition d’améliorations du fonctionnement de la ligne.</li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    id: 1,
+    type: "title",
+    title: "2013",
+  },
+  {
+    id: 0,
+    type: "card",
+    date: "2013",
+    title: "Chef de restauration",
+    company: "école municipale (Vaulx, Haute Savoie)",
+    content: (
+      <>
+        <ul>
+          <li>- Élaboration des menus.</li>
+          <li>- Gestion des stocks.</li>
+          <li>- Relations fournisseurs.</li>
+        </ul>
+      </>
+    ),
+  },
+];
 
 const WorksPage = (props) => {
     const {} = props
