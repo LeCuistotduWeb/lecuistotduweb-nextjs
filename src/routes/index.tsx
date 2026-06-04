8883f3ab5b40a452aafc8930a8c9754f5c585b63
lecuistotduweb <gaetan.boyron@gmail.com>
2026-06-04 19:29:44 +0200

    feat: ajouter des animations d'entrée aux cartes et simplifier les gradients dans le style

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -16,18 +16,18 @@ function Home() {
 		<main className="min-h-screen p-4 md:p-10">
 			<div className="mx-auto max-w-6xl">
 				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-4">
-					<BioCard         className="col-span-1 sm:col-span-2 md:col-span-3" />
-					<AvatarCard      className="col-span-1" />
-					<div className="col-span-1 flex flex-col gap-3">
+					<BioCard         className="col-span-1 sm:col-span-2 md:col-span-3 rise-in" />
+					<AvatarCard      className="col-span-1 rise-in [animation-delay:80ms]" />
+					<div className="col-span-1 flex flex-col gap-3 rise-in [animation-delay:160ms]">
 						<LinkedInCard />
 						<CvCard />
 					</div>
-					<ExperiencesCard className="col-span-1 md:col-span-2" />
-					<div className="col-span-1 flex flex-col gap-3">
+					<ExperiencesCard className="col-span-1 md:col-span-2 rise-in [animation-delay:240ms]" />
+					<div className="col-span-1 flex flex-col gap-3 rise-in [animation-delay:320ms]">
 						<StackCard />
 						<SocialCard />
 					</div>
-					<ProjectCard     className="col-span-1 sm:col-span-2 md:col-span-4 row-span-3" />
+					<ProjectCard     className="col-span-1 sm:col-span-2 md:col-span-4 row-span-3 rise-in [animation-delay:400ms]" />
 				</div>
 			</div>
 		</main>
